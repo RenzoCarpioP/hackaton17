a0313120fc1d5729303017eab165db05ff3e5162
Renzo Carpio <renzocarpiop@outlook.com>
2023-11-15 10:57:34 -0500

    hackaton azure cambio de puerto 2

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/', function(req, res) {
 });
 
 
-const port = process.env.PORT || 8081;
+const port = 8080;
 const server = https.createServer(httpsOptions, app)
     .listen(port, () => {
         console.log('server running at ' + port)
